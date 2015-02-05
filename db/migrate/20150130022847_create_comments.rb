d25ca06ac13c22f8a264bbbd5be96f7e90669ec5
MAuricio <martinezmauricio@msn.com>
2015-02-05 16:36:23 +0000

    fixed body id issue

--- db/migrate/20150130022847_create_comments.rb
+++ db/migrate/20150130022847_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
       t.text :body
-      t.references :body, index: true
+      t.references :post, index: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150202213002) do
+ActiveRecord::Schema.define(version: 20150130022847) do
 
   create_table "comments", force: true do |t|
     t.text     "body"
-    t.integer  "body_id"
+    t.integer  "post_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "post_id"
   end
 
-  add_index "comments", ["body_id"], name: "index_comments_on_body_id"
   add_index "comments", ["post_id"], name: "index_comments_on_post_id"
 
   create_table "posts", force: true do |t|
